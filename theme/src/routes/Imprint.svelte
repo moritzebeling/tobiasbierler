4e46e4f45eee4992d47154841445b2cb4bd79872
moritzebeling <git@moritzebeling.com>
2021-01-19 11:08:14 +0100

    dynamic contact info from cms

--- theme/src/routes/Imprint.svelte
+++ theme/src/routes/Imprint.svelte
@@ -1,15 +1,16 @@
 <script>
 
+    export let contact;
+
 </script>
 
 <aside>
 
-    <h1>Impressum (gemäß §5 TMG)</h1>
+    <h1>{contact.name}</h1>
 
-    <p>Tobias Bierler<br />
-    tobiasbierler@icloud.com</p>
-    <p>Trierer Straße 62<br />
-    99423 Weimar</p>
+    <p>{contact.email}<br />
+    {contact.street}<br />
+    {contact.zip} {contact.city}</p>
 
     <p>Die Inhalte dieser Webseite wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p>
 
--- theme/src/routes/Index.svelte
+++ theme/src/routes/Index.svelte
@@ -29,7 +29,7 @@
         </Link>
     </Grid>
 
-    <Imprint />
+    <Imprint contact={data.contact} />
 
 </main>
 
