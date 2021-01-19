10a484377272f9dd72f72b7539bb363ca03037f2
moritzebeling <git@moritzebeling.com>
2021-01-19 11:56:21 +0100

    add favicon

--- index.php
+++ index.php
@@ -22,6 +22,13 @@ $themeDirectory = '/theme/public';
     </script>
 
 	<script defer src='<?= $themeDirectory ?>/build/bundle.js'></script>
+
+	<link rel="icon" type="image/svg+xml" href="/theme/public/images/tobiasbierler.svg">
+	<link rel="icon" type="image/png" href="/theme/public/images/tobiasbierler-128.png" sizes="128x128">
+	<link rel="icon" type="image/png" href="/theme/public/images/tobiasbierler-256.png" sizes="256x256">
+	<link rel="icon" type="image/png" href="/theme/public/images/tobiasbierler-512.png" sizes="512x512">
+	<link rel="icon" type="image/png" href="/theme/public/images/tobiasbierler-1024.png" sizes="1024x1024">
+
 </head>
 
 <body>
